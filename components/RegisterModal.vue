0c9fd6068ba57a7f22fab6b7f87bebf6260baa4a
sunyll <s.fuentes.bongenaar@protonmail.com.>
2024-09-07 16:45:54 +0200

    Small adjustments registermodal, buttons should be correctly styled now

--- components/RegisterModal.vue
+++ components/RegisterModal.vue
@@ -2,12 +2,9 @@
   <GenericModal v-model="isOpen" title="Register for a new account">
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
       <form class="space-y-6" action="#" method="POST">
+        <!-- Email Input -->
         <div>
-          <label
-            for="email"
-            class="block text-sm font-medium leading-6 text-gray-300"
-            >Email address</label
-          >
+          <label for="email" class="block text-sm font-medium leading-6 text-gray-300">Email address</label>
           <div class="mt-2">
             <UInput
               id="email"
@@ -18,17 +15,15 @@
               type="email"
               autocomplete="email"
               required
-              class="block w-full rounded-md border-0 py-1.5 bg-gray-700 text-white shadow-sm ring-1 ring-inset ring-gray-600 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+              class="block w-full rounded-md border-0 py-1.5 bg-gray-700 text-white shadow-sm ring-1 ring-inset ring-gray-600 placeholder:text-gray-400 focus:bg-gray-700 focus:text-white focus:ring-gray-600 sm:text-sm sm:leading-6"
             />
           </div>
         </div>
+
+        <!-- Password Input -->
         <div>
           <div class="flex items-center justify-between">
-            <label
-              for="password"
-              class="block text-sm font-medium leading-6 text-gray-300"
-              >Password</label
-            >
+            <label for="password" class="block text-sm font-medium leading-6 text-gray-300">Password</label>
           </div>
           <div class="mt-2">
             <UInput
@@ -44,61 +39,56 @@
             />
           </div>
         </div>
+
+        <!-- Confirm Password Input -->
         <div>
           <div class="flex items-center justify-between">
-            <label
-              for="password"
-              class="block text-sm font-medium leading-6 text-gray-300"
-              >Confirm Password</label
-            >
+            <label for="confirm-password" class="block text-sm font-medium leading-6 text-gray-300">Confirm Password</label>
           </div>
           <div class="mt-2">
             <UInput
               v-model="confirmPassword"
               color="violet"
               variant="none"
-              name="password"
+              name="confirm-password"
               type="password"
               autocomplete="current-password"
               required
               class="block w-full rounded-md border-0 py-1.5 bg-gray-700 text-white shadow-sm ring-1 ring-inset ring-gray-600 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
             />
           </div>
-          <div
-            v-if="password !== confirmPassword"
-            class="flex items-center justify-between"
-          >
-            <label
-              for="password"
-              class="block text-sm font-medium leading-6 text-red-300"
-              >{{ passwordConfirmHint }}</label
-            >
+          <div v-if="password !== confirmPassword" class="flex items-center justify-between">
+            <label for="password" class="block text-sm font-medium leading-6 text-red-300">{{ passwordConfirmHint }}</label>
           </div>
         </div>
+
+        <!-- Sign Up Button -->
         <div>
           <UButton
             type="submit"
             color="violet"
-            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            class="flex w-full justify-center rounded-md bg-violet-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-600 disabled:opacity-50 disabled:cursor-not-allowed"
             :disabled="isDisabled"
             @click="submitRegistration"
           >
-            Sign in
+            Sign up
           </UButton>
         </div>
       </form>
     </div>
-    <div class="my-4 text-center">
-      <UButton
-        class="text-gray-400 hover:text-gray-200"
-        color="violet"
-        @click="isOpen = false"
-      >
-        Close
-      </UButton>
-    </div>
+
+    <!-- Close Button -->
+      <div class="mt-10 text-center">
+        <UButton 
+          @click="isOpen = false" 
+          class="flex justify-center items-center w-1/2 mx-auto rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+        >
+          Close
+        </UButton>
+      </div>
   </GenericModal>
 </template>
+
 
 <script lang="ts" setup>
 const email = ref("");
