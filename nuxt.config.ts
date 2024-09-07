7b81528b94325aec6284e677c1d1adf2ed1140b2
acrobid <116527022+acrobid@users.noreply.github.com>
2024-09-07 16:38:31 +0900

    landing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  image: {
+    provider: "ipx",
+  },
 
   routeRules: {
     // prerender index route by default
     "/": { prerender: true, redirect: "/LandingPage" },
   },
 
-  modules: ["@nuxt/ui", "@nuxt/eslint"],
+  modules: ["@nuxt/ui", "@nuxt/eslint", "@nuxt/image"],
 });
