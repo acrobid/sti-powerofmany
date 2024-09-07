f13c77bdf44f3421d7f2caca8ef899967e038c96
acrobid <116527022+acrobid@users.noreply.github.com>
2024-09-07 20:39:19 +0900

    campaign user list

--- pages/UserInfo.vue
+++ pages/UserInfo.vue
@@ -1,13 +1,9 @@
 <template>
-  <div>
-    
+  <div class="mx-auto max-w-7xl sm:px-6 lg:px-8 space-y-4">
+    <OrganizerInfoCard />
+    <OrganizationSummaryCard />
+    <CampaignUserList />
   </div>
 </template>
 
-<script lang="ts" setup>
-
-</script>
-
-<style>
-
-</style>
+<script setup></script>
