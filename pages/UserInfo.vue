a32604c86df23d31974ac7078f38021458d8f2d7
acrobid <116527022+acrobid@users.noreply.github.com>
2024-09-07 22:08:49 +0900

    userinfo

--- pages/UserInfo.vue
+++ pages/UserInfo.vue
@@ -1,9 +1,20 @@
 <template>
-  <div class="mx-auto max-w-7xl sm:px-6 lg:px-8 space-y-4">
-    <OrganizerInfoCard />
-    <OrganizationSummaryCard />
-    <CampaignUserList />
+  <div class="relative bg-gray-900 py-24 sm:py-32">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+      <div class="mx-auto max-w-7xl sm:px-6 lg:px-8 space-y-4">
+        <OrganizerInfoCard />
+        <div v-if="campaignExists">
+          <OrganizationSummaryCard />
+          <CampaignUserList />
+        </div>
+        <div v-else>
+          <CreateCampaign />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+const campaignExists = ref(true);
+</script>
