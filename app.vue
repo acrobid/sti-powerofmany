e1b1ffc0f33fa09cc2777e8afafe042bf426e7e6
acrobid <116527022+acrobid@users.noreply.github.com>
2024-09-08 04:48:26 +0900

    fix?

--- app.vue
+++ app.vue
@@ -5,3 +5,50 @@
     <SiteFooter />
   </div>
 </template>
+
+<script setup lang="ts">
+useHead({
+  link: [
+    {
+      rel: "apple-touch-icon",
+      sizes: "152x152",
+      href: "/apple-icon-152x152.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      href: "/apple-icon-180x180.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "192x192",
+      href: "/android-icon-192x192.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      href: "/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "96x96",
+      href: "/favicon-96x96.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      href: "/favicon-16x16.png",
+    },
+    { rel: "manifest", href: "/manifest.json" },
+  ],
+  meta: [
+    { name: "msapplication-TileColor", content: "#ffffff" },
+    { name: "msapplication-TileImage", content: "/ms-icon-144x144.png" },
+    { name: "theme-color", content: "#ffffff" },
+  ],
+});
+</script>
