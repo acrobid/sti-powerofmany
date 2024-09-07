dcd7def0f29a81dde37db5a0eb7a34a1d85b6d56
acrobid <116527022+acrobid@users.noreply.github.com>
2024-09-08 00:14:40 +0900

    change login status

--- components/SiteHeader.vue
+++ components/SiteHeader.vue
@@ -11,7 +11,7 @@
           <div class="text-2xl font-bold">Form a union</div>
         </div>
 
-        <div v-if = "isLogedIn" >
+        <div v-if="!isLogedIn">
           <UButton
             color="violet"
             class="px-4 py-2 rounded mr-2"
@@ -27,11 +27,14 @@
             Sign Up
           </UButton>
         </div>
-        <button v-else class="bg-violet-600 hover:bg-violet-700 text-white px-4 py-2 rounded">
+        <button
+          v-else
+          class="bg-violet-600 hover:bg-violet-700 text-white px-4 py-2 rounded"
+        >
           Logout
         </button>
       </div>
-      
+
       <LoginModal v-model="loginModal" @register="registerModal = true" />
       <RegisterModal v-model="registerModal" />
     </nav>
