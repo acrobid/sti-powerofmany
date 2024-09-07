fdb2616c712617b93433343a7d1584714458ee24
sunyll <s.fuentes.bongenaar@protonmail.com.>
2024-09-07 09:52:31 +0200

    Pasted the NonUserUnion code in the CampaignSignup page, this was the correct one.

--- pages/NonUserUnion.vue
+++ pages/NonUserUnion.vue
@@ -8,24 +8,10 @@
      UNION_NAME
   </div>
   <div>
-    <UInput v-model="nonuser_name" size="sm" color="white" placeholder="Name" />
-    <UInput v-model="department" size="sm" color="white" placeholder="Department" />
-    <UInput v-model="job_title" size="sm" color="white" placeholder="Job title" />
-    <UInput v-model="employee_id" size="sm" color="white" placeholder="Employee Id" />
-    <UInput v-model="email" size="sm" color="white" placeholder="Email address" />
-    <UInput v-model="phone_number" size="sm" color="white" placeholder="Phone number" />
-    <UButton>Submit and Sign</UButton> <!-- N2: Should redirect to completion page -->
   </div>
 </template>
 
 <script lang="ts" setup>
-const nonuser_name = ref('')
-const employee_id = ref('')
-const job_title = ref('')
-const department = ref('')
-const email = ref('')
-const phone_number = ref('')
-
 </script>
 
 <style>
